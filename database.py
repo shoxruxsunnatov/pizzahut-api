6fd85020c3b66d6a3e31c3cc5deb74f751745e1f
Shoxrux Sunnatov <orozboyjigit@gmail.com>
2023-05-31 18:20:54 +0500

    Initial commit

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('postgresql://p10:123@localhost/pizza_db', echo=True)
+engine = create_engine('postgresql://postgres:12345@localhost/pizzehut', echo=True)
 
 Base = declarative_base()
 
